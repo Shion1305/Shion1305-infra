264a73365752ec4741676b0360afb0da966195a8
Shion Ichikawa <shion1305@gmail.com>
2024-11-19 21:20:37 +0900

    🧱 remove oci_compute_instance-2021

--- oci/imports.tf
+++ oci/imports.tf
@@ -9,11 +9,6 @@ import {
 }
 
 import {
-  id = "ocid1.instance.oc1.ap-tokyo-1.anxhiljrqiqkkcycvitxf4ct2cvw2bpjadulub3eamyg63sgsotbjzs36raq"
-  to = oci_core_instance.instance-2021
-}
-
-import {
   id = "ocid1.subnet.oc1.ap-tokyo-1.aaaaaaaaneghxjqpturxbnptzpcf44egzgd2gzg3el2oxyum2lj3ygczbkeq"
   to = oci_core_subnet.main-subnet
 }
