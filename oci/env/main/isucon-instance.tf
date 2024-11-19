1c4a5354aa379ffa7f3761588ba566fdbeb9cf30
Shion Ichikawa <shion1305@gmail.com>
2024-11-20 04:13:53 +0900

    🧱 [isucon] deploy 3 instances

--- oci/env/main/isucon-instance.tf
+++ oci/env/main/isucon-instance.tf
@@ -1,5 +1,21 @@
-module "test-instance" {
-  name           = "test-instance"
+module "isucon-1" {
+  name           = "isucon-1"
+  source         = "../../modules/isucon/instance"
+  compartment-id = oci_identity_compartment.main-compartment.id
+  subnet_id      = oci_core_subnet.isucon-default.id
+  ssh_keys       = var.isucon-ssh-keys
+}
+
+module "isucon-2" {
+  name           = "isucon-2"
+  source         = "../../modules/isucon/instance"
+  compartment-id = oci_identity_compartment.main-compartment.id
+  subnet_id      = oci_core_subnet.isucon-default.id
+  ssh_keys       = var.isucon-ssh-keys
+}
+
+module "isucon-3" {
+  name           = "isucon-3"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
