2fe60f2494d2196298ebce7c98cc55772d4bdd0a
Shion Ichikawa <shion1305@gmail.com>
2024-11-22 02:10:18 +0900

    🧱 update isucon instances

--- oci/env/main/isucon-instance.tf
+++ oci/env/main/isucon-instance.tf
@@ -1,24 +1,26 @@
 module "isu1" {
-  name           = "isu1"
+  name           = "isucon10q-updated"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
-  user_data      = file("../../modules/isucon/instance/cloud-init/isucon11q.cfg")
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon10q.cfg"))
 }
 
 module "isu2" {
-  name           = "isu2"
+  name           = "isucon11q"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon11q.cfg"))
 }
 
 module "isu3" {
-  name           = "isu3"
+  name           = "private-isu-updated"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/private-isu/app.cfg"))
 }
