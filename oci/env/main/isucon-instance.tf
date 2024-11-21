7abb6875ecbd0a91aed5552d5c7d5f06af63f83e
Shion Ichikawa <shion1305@gmail.com>
2024-11-22 03:42:21 +0900

    🧱 Update infra for private-isu

--- oci/env/main/isucon-instance.tf
+++ oci/env/main/isucon-instance.tf
@@ -1,26 +1,25 @@
-module "isu1" {
-  name           = "isucon10q-updated"
-  source         = "../../modules/isucon/instance"
-  compartment-id = oci_identity_compartment.main-compartment.id
-  subnet_id      = oci_core_subnet.isucon-default.id
-  ssh_keys       = var.isucon-ssh-keys
-  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon10q.cfg"))
-}
-
-module "isu2" {
-  name           = "isucon11q"
-  source         = "../../modules/isucon/instance"
-  compartment-id = oci_identity_compartment.main-compartment.id
-  subnet_id      = oci_core_subnet.isucon-default.id
-  ssh_keys       = var.isucon-ssh-keys
-  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon11q.cfg"))
-}
-
-module "isu3" {
-  name           = "private-isu-updated"
+module "private-isu" {
+  name           = "private-isu"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
   user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/private-isu/app.cfg"))
+}
+
+module "private-isu2" {
+  name           = "isucon10q-updated"
+  source         = "../../modules/isucon/instance"
+  compartment-id = oci_identity_compartment.main-compartment.id
+  subnet_id      = oci_core_subnet.isucon-default.id
+  ssh_keys       = var.isucon-ssh-keys
+}
+
+module "private-isu-bench" {
+  name           = "isucon11q"
+  source         = "../../modules/isucon/instance"
+  compartment-id = oci_identity_compartment.main-compartment.id
+  subnet_id      = oci_core_subnet.isucon-default.id
+  ssh_keys       = var.isucon-ssh-keys
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/private-isu/benchmarker.cfg"))
 }
