c2492f49d7a834919d0533ee3ef3481b43bdf3ce
Shion Ichikawa <shion1305@gmail.com>
2024-11-22 02:08:56 +0900

    🔧 update source_id default to ubuntu22.04

--- oci/modules/isucon/instance/variable.tf
+++ oci/modules/isucon/instance/variable.tf
@@ -16,7 +16,7 @@ variable "subnet_id" {
 variable "source_id" {
   type        = string
   description = "Source ID"
-  default     = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaaboz4eqdgjyzpzu55b3wgxrqpqzonugsja5jhbr6wqewngq3576oa"
+  default     = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaadcb75lsmgfzxtrirj6l7rb7ecd4ivz7fwzi2py2kk2molifncsia"
 }
 
 variable "ssh_keys" {
