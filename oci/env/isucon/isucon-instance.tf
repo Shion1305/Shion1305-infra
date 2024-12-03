676bbf835b85ea245205d3710cd9eda5f139355b
Shion Ichikawa <shion1305@gmail.com>
2024-12-03 21:49:10 +0900

    🧱 isucon13への切り替え

--- oci/env/isucon/isucon-instance.tf
+++ oci/env/isucon/isucon-instance.tf
@@ -1,15 +1,15 @@
-module "private-isu1-1202" {
-  name           = "private-isu1-1202"
+module "isucon13-1203-1" {
+  name           = "isucon13-1203-1"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
-  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/private-isu/app.cfg"))
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon13.cfg"))
   private_ip = "10.0.0.11"
 }
 
-module "private-isu2-1202" {
-  name           = "private-isu2-1202"
+module "isucon13-1203-2" {
+  name           = "isucon13-1203-2"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
@@ -17,12 +17,12 @@ module "private-isu2-1202" {
   private_ip = "10.0.0.12"
 }
 
-module "private-isu-bench-1202" {
-  name           = "private-isu-bench-1202"
+module "isucon13-1203-3" {
+  name           = "isucon13-1203-3"
   source         = "../../modules/isucon/instance"
   compartment-id = oci_identity_compartment.main-compartment.id
   subnet_id      = oci_core_subnet.isucon-default.id
   ssh_keys       = var.isucon-ssh-keys
-  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/private-isu/benchmarker.cfg"))
+  user_data      = base64encode(file("../../modules/isucon/instance/cloud-init/isucon13.cfg"))
   private_ip = "10.0.0.13"
 }
