63ccab9051f997289c4e3fea8fb4754e366cc0de
Shion Ichikawa <shion1305@gmail.com>
2024-12-22 06:22:46 +0900

    ✨ create new instance using [main/compute] module

---
+++ oci/env/main/oci_compute_instance-2024.tf
@@ -0,0 +1,15 @@
+module "oci_compute_instance-2024" {
+  source                    = "../../modules/main/compute"
+  subnet_id                 = oci_core_subnet.main-subnet.id
+  private_ip                = "10.0.0.20"
+  compartment_id            = oci_identity_compartment.main-compartment.id
+  ssh_authorized_keys       = var.ssh_keys
+  assign_private_dns_record = true
+  assign_public_ip          = true
+  display_name              = "oci_compute_instance-2024-1"
+  ocpus                     = 1
+  memory_in_gbs             = 6
+  shape                     = "VM.Standard.A1.Flex"
+  fault_domain              = "FAULT-DOMAIN-1"
+  availability_domain       = "kbTA:AP-TOKYO-1-AD-1"
+}
