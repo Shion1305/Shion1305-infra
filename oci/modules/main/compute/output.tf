eb1f80eefc0b991e2b0b759bdfe4f77af860216b
Shion Ichikawa <shion1305@gmail.com>
2024-12-22 06:45:15 +0900

    🐛 [module/main/compute] output.tf

--- oci/modules/main/compute/output.tf
+++ oci/modules/main/compute/output.tf
@@ -1,19 +1,19 @@
-output "instance_public_ip" {
-  value       = ""
+output "public_ip" {
+  value       = oci_core_instance.this.public_ip
   description = "The public IP address of the EC2 instance"
 }
 
-output "instance_ocid" {
+output "ocid" {
   description = "The OCID of the created instance"
   value       = oci_core_instance.this.id
 }
 
-output "instance_private_ip" {
+output "private_ip" {
   value       = oci_core_instance.this.private_ip
   description = "The private IP address of the EC2 instance"
 }
 
-output "instance_hostname" {
-  value       = oci_core_instance.this.create_vnic_details.hostname_label
+output "hostname" {
+  value       = oci_core_instance.this.create_vnic_details[0].hostname_label
   description = "The DNS name of the EC2 instance"
 }
