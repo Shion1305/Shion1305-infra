1d4d6aea204a9af1fd3a50a8032ccf8cec147e98
Shion Ichikawa <shion1305@gmail.com>
2025-10-29 04:03:02 +0900

    🔥 Remove deprecated import for oci_core_instance.instance-2022

--- oci/env/main/imports.tf
+++ oci/env/main/imports.tf
@@ -1,9 +1,4 @@
 import {
-  id = "ocid1.instance.oc1.ap-tokyo-1.anxhiljrqiqkkcycklw5l5plncwm7nj64rdqpga6zfe67gsn2zzouexqnlwa"
-  to = oci_core_instance.instance-2022
-}
-
-import {
   id = "ocid1.subnet.oc1.ap-tokyo-1.aaaaaaaaneghxjqpturxbnptzpcf44egzgd2gzg3el2oxyum2lj3ygczbkeq"
   to = oci_core_subnet.main-subnet
 }
