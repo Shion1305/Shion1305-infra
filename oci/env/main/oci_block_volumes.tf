7291d5cee59d5fb3ce9b8267462f93bb85c41849
Shion Ichikawa <shion1305@gmail.com>
2026-03-07 01:14:46 +0900

    feat(k8s, k8s-proxy): attach 50GB block volume

---
+++ oci/env/main/oci_block_volumes.tf
@@ -0,0 +1,29 @@
+resource "oci_core_volume" "k8s-block-volume" {
+  compartment_id      = oci_identity_compartment.main-compartment.id
+  availability_domain = "kbTA:AP-TOKYO-1-AD-1"
+  display_name        = "k8s-block-volume"
+  size_in_gbs         = 50
+  vpus_per_gb         = 0
+}
+
+resource "oci_core_volume" "k8s-proxy-block-volume" {
+  compartment_id      = oci_identity_compartment.main-compartment.id
+  availability_domain = "kbTA:AP-TOKYO-1-AD-1"
+  display_name        = "k8s-proxy-block-volume"
+  size_in_gbs         = 50
+  vpus_per_gb         = 0
+}
+
+resource "oci_core_volume_attachment" "k8s-block-volume-attachment" {
+  attachment_type = "paravirtualized"
+  instance_id     = module.oci_compute_instance-k8s.ocid
+  volume_id       = oci_core_volume.k8s-block-volume.id
+  display_name    = "k8s-block-volume-attachment"
+}
+
+resource "oci_core_volume_attachment" "k8s-proxy-block-volume-attachment" {
+  attachment_type = "paravirtualized"
+  instance_id     = module.oci_compute_instance-k8s-proxy.ocid
+  volume_id       = oci_core_volume.k8s-proxy-block-volume.id
+  display_name    = "k8s-proxy-block-volume-attachment"
+}
